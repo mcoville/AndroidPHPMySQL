1a96f8070dd667c82c9bf3de235bf70b3d60166b
Mike Coville <coville@gmail.com>
2022-09-02 12:14:11 -0400

    Initial commit

---
+++ app/src/main/java/com/example/androidphpmysql/Constants.java
@@ -0,0 +1,8 @@
+package com.example.androidphpmysql;
+
+public class Constants {
+
+    private static final String ROOT_URL = "http://192.168.1.5/Android/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+}
